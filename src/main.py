797e288dcc6ed9b5ec205e05a5b42d52004187ee
ArchieG1237 <archieheadland@hotmail.co.uk>
2024-09-09 00:13:13 +0100

    made it better

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 import pygame
 from pygame.locals import *
 
+
 pygame.init()
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 900
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Images
+black_bishop = pygame.transform.scale(pygame.image.load("Sprites/bishop-b.svg"), (250,250))
+chess_board = pygame.transform.scale(pygame.image.load("Sprites/chess-board.png"), (0.8*SCREEN_WIDTH, 0.8*SCREEN_HEIGHT))
 
 running = 1
 
@@ -15,6 +20,8 @@ while running:
 
 
   screen.fill((120, 120, 120))
+  screen.blit(chess_board, (100, 100))
+  screen.blit(black_bishop, (100, 100))
   pygame.display.flip()
 
 pygame.quit()
