8c174a95a971186010c4a10a0107d295ca891f18
isaac allinson <isaacallinson@hotmail.co.uk>
2024-09-10 01:42:04 +0100

    Update piece.py

--- src/piece.py
+++ src/piece.py
@@ -1,11 +1,51 @@
-# Player 0 = White 1 = Black
+# Player: -1 = White 1 = Black
+
+spriteFiles = {
+    "Pawn":
+}
 
 class Piece:
 
-    def __init__(self,spriteFn,player,coord):
-        self.sprite = spriteFn
+    def __init__(self,player,coord):
         self.player = player
         self.coord = coord
 
     def checkMove(self, destination):
         pass
+
+
+class Pawn(Piece):
+    def __init__(self, spriteFn, player, coord):
+        super().__init__(spriteFn, player, coord)
+
+
+
+class Knight(Piece):
+    def __init__(self, spriteFn, player, coord):
+        super().__init__(spriteFn, player, coord)
+
+
+
+class Bishop(Piece):
+    def __init__(self, spriteFn, player, coord):
+        super().__init__(spriteFn, player, coord)
+
+
+
+class Rook(Piece):
+    def __init__(self, spriteFn, player, coord):
+        super().__init__(spriteFn, player, coord)
+
+
+
+class Queen(Piece):
+    def __init__(self, spriteFn, player, coord):
+        super().__init__(spriteFn, player, coord)
+
+
+
+class King(Piece):
+    def __init__(self, spriteFn, player, coord):
+        super().__init__(spriteFn, player, coord)
+
+        
