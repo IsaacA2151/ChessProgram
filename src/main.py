c0e3bd179bf7cfecd6c1936fa4afc6966eefd918
ArchieG1237 <archieheadland@hotmail.co.uk>
2024-09-10 02:01:25 +0100

    MUSIC

--- src/main.py
+++ src/main.py
@@ -7,7 +7,13 @@ SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 900
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# Images
+
+Music = pygame.mixer.Sound("src/Sprites/Music.Mp3")
+Music.set_volume(0.03)
+Music.play()
+
+
+# Image
 black_bishop = pygame.image.load("src/Sprites/bishop-b.png")
 chess_board = pygame.image.load("src/Sprites/chess-board.png")
 
@@ -18,10 +24,11 @@ while running:
     if event.type == QUIT:
       running = 0
 
-
+  mouse_x, mouse_y = pygame.mouse.get_pos() # Get coordinates of mouse cursor
   screen.fill((100, 100, 100))
-  screen.blit(chess_board, (100, 100))
-  screen.blit(black_bishop, (100, 100))
+  screen.blit(chess_board, (91, 90))
+  screen.blit(black_bishop, (90, 90))
   pygame.display.flip()
+  
 
 pygame.quit()
